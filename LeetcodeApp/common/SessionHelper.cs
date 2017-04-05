ba354ae91e50b132470409f83d692e7e1563c8fd
Zhou LuRong <133130919@qq.com>
2017-04-05 13:06:10 +0800

    2017/4/5

---
+++ LeetcodeApp/common/SessionHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace LeetcodeApp.common
+{
+    /// <summary>
+    /// Session 操作类
+    /// 1、GetSession(string name)根据session名获取session对象
+    /// 2、SetSession(string name, object val)设置session
+    /// </summary>
+    public class SessionHelper
+    {
+        /// <summary>
+        /// 根据session名获取session对象
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static object GetSession(string name)
+        {
+            return HttpContext.Current.Session[name];
+        }
+
+        /// <summary>
+        /// 设置session
+        /// </summary>
+        /// <param name="name">session 名</param>
+        /// <param name="val">session 值</param>
+        public static void SetSession(string name, object val)
+        {
+            HttpContext.Current.Session.Remove(name);
+            HttpContext.Current.Session.Add(name, val);
+        }
+    }
+}
